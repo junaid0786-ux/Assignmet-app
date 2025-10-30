034fbf50eab462b3bb58427ec9d9528067fd8989
Junaid Khan <junaid9068445457@gmail.com>
2025-10-30 19:21:27 +0530

    organised code

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import React from 'react'
-import Sidebar from './Components/Sidebar'
-import Home from './Page/Home'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import List from './Page/List'
-import Table from './Page/Table'
-import Form from './Page/Form'
-
+import React from "react";
+import Sidebar from "./Components/Sidebar";
+import Home from "./Page/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import List from "./Page/List";
+import Table from "./Page/Table";
+import Form from "./Page/Form";
 
 const App = () => {
   return (
     <>
-    <BrowserRouter>
-      <div className="flex">
-      
-        <Sidebar />
+      <BrowserRouter>
+        <div className="flex">
+          <Sidebar />
 
-      
-        <div className="ml-[25%] w-[75%] ">
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/List" element={<List />} />
-            <Route path="/Table" element={<Table />} />
-            <Route path="/Form" element={<Form />} />
-          </Routes>
+          <div className="ml-[25%] w-[75%] ">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/List" element={<List />} />
+              <Route path="/Table" element={<Table />} />
+              <Route path="/Form" element={<Form />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,14 +2,13 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-
-  const [selectedSection, setSelectedSection] = useState('');
+  const [selectedSection, setSelectedSection] = useState("");
 
   const handleSelectChange = (event) => {
     const sectionId = event.target.value;
     setSelectedSection(sectionId);
 
-    document.getElementById(sectionId)?.scrollIntoView({ behavior: 'smooth' });
+    document.getElementById(sectionId)?.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
@@ -17,16 +16,30 @@ const Sidebar = () => {
       <div className="bg-gray-100 w-full h-full rounded-2xl shadow-2xl flex-col justify-center items-center gap-1 overflow-hidden">
         <div className="bg-amber-300 w-full flex items-center justify-center p-4 ">
           <h1>
-            <b><Link to="/">Assignment</Link></b>
+            <b>
+              <Link to="/">Assignment</Link>
+            </b>
           </h1>
         </div>
         <div className=" flex flex-col w-full h-auto p-2 pt-5 gap-5 ">
-
           {/* tasks boxes */}
           <div className="bg-white flex items-center justify-between p-4 rounded-2xl">
-            <label htmlFor="list"><b> <Link to="/List">List <hr/></Link> </b> </label>
-            <div >
-              <select name="list" id="list" onChange={handleSelectChange} value={selectedSection} className="bg-amber-300 flex items-center justify-content-center rounded-full p-1 " >
+            <label htmlFor="list">
+              <b>
+                {" "}
+                <Link to="/List">
+                  List <hr />
+                </Link>{" "}
+              </b>{" "}
+            </label>
+            <div>
+              <select
+                name="list"
+                id="list"
+                onChange={handleSelectChange}
+                value={selectedSection}
+                className="bg-amber-300 flex items-center justify-content-center rounded-full p-1 "
+              >
                 <option value="">Select Task</option>
                 <option value="section1">Task 1:</option>
                 <option value="section2">Task 2:</option>
@@ -38,9 +51,19 @@ const Sidebar = () => {
           </div>
 
           <div className="bg-white flex items-center justify-between p-4 rounded-2xl">
-            <label htmlFor="list"><b><Link to="Table">Table <hr /></Link></b></label>
-            <div >
-              <select name="list" id="list" className="bg-amber-300 flex items-center justify-content-center rounded-full p-1" >
+            <label htmlFor="list">
+              <b>
+                <Link to="Table">
+                  Table <hr />
+                </Link>
+              </b>
+            </label>
+            <div>
+              <select
+                name="list"
+                id="list"
+                className="bg-amber-300 flex items-center justify-content-center rounded-full p-1"
+              >
                 <option value="list">Select Task</option>
                 <option value="list">Task 1:</option>
                 <option value="list">Task 2:</option>
@@ -50,9 +73,20 @@ const Sidebar = () => {
           </div>
 
           <div className="bg-white items-center flex justify-between p-4 rounded-2xl">
-            <label htmlFor="list"><b> <Link to="/Form">Form <hr /></Link></b></label>
-            <div >
-              <select name="list" id="list" className="bg-amber-300 flex items-center justify-content-center rounded-full p-1" >
+            <label htmlFor="list">
+              <b>
+                {" "}
+                <Link to="/Form">
+                  Form <hr />
+                </Link>
+              </b>
+            </label>
+            <div>
+              <select
+                name="list"
+                id="list"
+                className="bg-amber-300 flex items-center justify-content-center rounded-full p-1"
+              >
                 <option value="list">Select Task</option>
                 <option value="list">Task 1:</option>
                 <option value="list">Task 2:</option>
@@ -62,9 +96,15 @@ const Sidebar = () => {
           </div>
 
           <div className="bg-white items-center flex justify-between p-4 rounded-2xl">
-            <label htmlFor="list"><b>Lists</b></label>
-            <div >
-              <select name="list" id="list" className="bg-amber-300 flex items-center justify-content-center rounded-full p-1" >
+            <label htmlFor="list">
+              <b>Lists</b>
+            </label>
+            <div>
+              <select
+                name="list"
+                id="list"
+                className="bg-amber-300 flex items-center justify-content-center rounded-full p-1"
+              >
                 <option value="list">Select Task</option>
                 <option value="list">Task 1:</option>
                 <option value="list">Task 2:</option>
@@ -72,7 +112,6 @@ const Sidebar = () => {
               </select>
             </div>
           </div>
-
         </div>
       </div>
     </div>
