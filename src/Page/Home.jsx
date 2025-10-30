a9cb617a41c3fb674441e94776ef500fd06106e8
Junaid Khan <junaid9068445457@gmail.com>
2025-10-30 19:19:19 +0530

    home done

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -7,15 +7,15 @@ const Home = () => {
       className="relative w-full h-screen bg-fixed bg-cover bg-center overflow-y-auto "
       style={{ backgroundImage: `url(${image})` }}
     >
-      <div className="bg-amber-300/60 h-full w-full flex items-center justify-center ">
+      <div className="bg-amber-200/60 h-full w-full flex items-center justify-center ">
         <div>
           <h1
             style={{
               fontFamily: "'Fredoka', sans-serif",
-              color: "#FFFFFF",
-              textShadow: "3px 3px 10px #000",
+              color: "#FFFDD0",
+              textShadow: "5px 5px 10px #FFFF00",
             }}
-            className="animate-motion  text-7xl font-bold  drop-shadow-[4px_4px_20px] tracking-widest uppercase glass-text"
+            className="animate-motion  text-7xl font-bold  drop-shadow-[4px_4px_0px] tracking-widest uppercase glass-text"
           >
             <b>My Assignments</b>
           </h1>
